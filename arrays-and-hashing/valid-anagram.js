336d962bdd1f7d18aa523c39f3ec1f7d99d2f730
kevin <kevindbaik@gmail.com>
2024-02-13 12:38:51 -0800

    valid anagram

---
+++ arrays-and-hashing/valid-anagram.js
@@ -0,0 +1,64 @@
+// 242. Valid Anagram
+// Easy
+// https://leetcode.com/problems/valid-anagram/description/
+
+//* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+//* An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+/**
+ * * Example 1:
+ * * Input: s = "anagram", t = "nagaram"
+ * * Output: true
+
+* * Example 2:
+* * Input: s = "rat", t = "car"
+* * Output: false
+
+* * Constraints:
+* * 1 <= s.length, t.length <= 5 * 104
+* * s and t consist of lowercase English letters.
+*/
+
+// Method 1: Use on hash map
+const validAnagram = function(s,t) {
+  if(s.length !== t.length) return false;
+
+  const hash = {};
+  for(const e of s) {
+    if(hash[e]) hash[e]++;
+    else hash[e] = 1;
+  }
+
+  for(const e of t) {
+    if(!hash[e]) return false;
+    hash[e]--;
+    if(hash[e] === 0) delete hash[e];
+  }
+
+  return Object.keys(hash).length === 0;
+}
+
+// Method 2: Use two hash maps
+const validAnagram2 = function(s, t) {
+  if(s.length !== t.length) return false;
+
+  const hash1 = {};
+  const hash2 = {};
+
+  for(const e of s) {
+    if(hash1[e]) hash1[e]++;
+    else hash1[e] = 1;
+  }
+
+  for(const e of t) {
+    if(hash2[e]) hash2[e]++;
+    else hash2[e] = 1;
+  }
+
+  for(const key in hash1) {
+    if(!hash2[key] || hash1[key] !== hash2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
